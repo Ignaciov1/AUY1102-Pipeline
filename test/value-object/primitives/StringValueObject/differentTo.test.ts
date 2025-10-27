c3327808c70184aefb93f90b7f13639954d43669
ignacio <ig.sariego@duocuc.cl>
2025-10-27 15:18:47 -0300

    corregir workflow de SonarCloud con cobertura

--- test/value-object/primitives/StringValueObject/differentTo.test.ts
+++ test/value-object/primitives/StringValueObject/differentTo.test.ts
@@ -1,24 +1,38 @@
 import { WordMother } from '../../../../test/mother-object/WordMother';
 import FirstName from '../../../../test/value-object/primitives/StringValueObject/__mocks__/FirstName'
 
-let firstName: FirstName;
-let name: string;
+// Eliminamos las variables globales para evitar confusiones
+// let firstName: FirstName;
+// let name: string;
 
 describe('StringValueObject', () => {
-  beforeEach(() => {
-    name = WordMother.random()
-  })
+  // Eliminamos el 'beforeEach' porque generaba datos aleatorios impredecibles
 
   describe('different to', () => {
+
+    /**
+     * ESTA ES LA PRUEBA QUE ESTABA FALLANDO.
+     * La arreglamos usando valores fijos ("palabra_uno" y "palabra_dos")
+     * en lugar de valores aleatorios.
+     */
     it(`should return "true" if the values are different`, () => {
-      firstName = new FirstName(WordMother.random())
+      const firstName = new FirstName("palabra_uno");
+      const name = "palabra_dos"; // Un valor diferente
+      
       const expected = firstName.differentTo(name);
 
-      expect(expected).toBe(false);
+      // La prueba original estaba bien (esperar 'true'), el problema eran los datos.
+      expect(expected).toBe(true);
     })
 
+    /**
+     * Esta prueba ya estaba bien, pero la hacemos independiente
+     * para mayor claridad.
+     */
     it(`should return "false" if the values are not different`, () => {
-      firstName = new FirstName(name)
+      const name = WordMother.random(); // Creamos un valor
+      const firstName = new FirstName(name); // Usamos el MISMO valor
+      
       const expected = firstName.differentTo(name);
 
       expect(expected).toBe(false);
